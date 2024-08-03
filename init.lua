a83e6499c4ee1642c264d87a8f76976e3e4c2e18
Toplica Tanasković <toplicius@gmail.com>
2024-08-03 23:05:07 +0200

    Added intial welcome screen config

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ require "user.options"
 require "user.keymaps"
 require "user.autocmds"
 
+spec "user.alpha"
 spec "user.whichkey"
 spec "user.nvimtree"
 spec "user.lspconfig"
---
+++ lua/user/alpha.lua
@@ -0,0 +1,31 @@
+local M = {
+  "goolord/alpha-nvim",
+  event = "VimEnter",
+}
+
+function M.config()
+  local dashboard = require "alpha.themes.dashboard"
+
+  require("alpha").setup(dashboard.opts)
+
+  vim.api.nvim_create_autocmd("User", {
+    pattern = "LazyVimStarted",
+    callback = function()
+      local stats = require("lazy").stats()
+      local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+      dashboard.section.footer.val = "Loaded " .. stats.count .. " plugins in " .. ms .. "ms"
+      pcall(vim.cmd.AlphaRedraw)
+    end,
+  })
+
+  vim.api.nvim_create_autocmd({ "User" }, {
+    pattern = { "AlphaReady" },
+    callback = function()
+      vim.cmd [[
+      set laststatus=0 | autocmd BufUnload <buffer> set laststatus=3
+    ]]
+    end,
+  })
+end
+
+return M
