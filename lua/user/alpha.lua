e7f16bd1aeefe78d6ed5ada6e6865ff56e939cef
Toplica Tanasković <toplicius@gmail.com>
2024-08-04 13:40:33 +0200

    Configured welcome screen

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -6,17 +6,66 @@ local M = {
 function M.config()
   local dashboard = require "alpha.themes.dashboard"
 
+  dashboard.section.header.val = {
+    [[          ██╗   ██╗███╗   ██╗██╗██╗  ██╗]],
+    [[          ██║   ██║████╗  ██║██║╚██╗██╔╝]],
+    [[          ██║   ██║██╔██╗ ██║██║ ╚███╔╝ ]],
+    [[          ██║   ██║██║╚██╗██║██║ ██╔██╗ ]],
+    [[           ██████╔╝██║ ╚████║██║██╔╝ ██╗]],
+    [[           ╚═════╝ ╚═╝  ╚═══╝╚═╝╚═╝  ╚═╝]],
+    [[]],
+    [[                        ██╗    ]],
+    [[                        ██║    ]],
+    [[                    ██████████╗]],
+    [[                    ╚═══██╔═══╝]],
+    [[                        ██║    ]],
+    [[                        ╚═╝    ]],
+    [[]],
+    [[███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗]],
+    [[████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║]],
+    [[██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║]],
+    [[██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║]],
+    [[██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║]],
+    [[╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
+    [[                                                  ]],
+    [[     Where there is a shell, there is a way.      ]],
+  }
+
+  local footer = {
+    [[]],
+    [[The computing scientist’s main challenge is not to]],
+    [[get confused by the complexities of his own making.]],
+    [[]],
+    [[                              -- Edsger W. Dijkstra]],
+  }
+
+  dashboard.section.footer.val = footer
+
+  dashboard.section.buttons.val = {
+    dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+    dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+    dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
+    dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
+    dashboard.button("t", "󱎸  Find text", ":Telescope live_grep <CR>"),
+    dashboard.button("c", "  Configuration", ":e $MYVIMRC <CR>"),
+    dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+  }
+
+  dashboard.section.header.opts.hl = "Keyword"
+  dashboard.section.buttons.opts.hl = "Include"
+  dashboard.section.footer.opts.hl = "Type"
+
   require("alpha").setup(dashboard.opts)
 
-  vim.api.nvim_create_autocmd("User", {
-    pattern = "LazyVimStarted",
-    callback = function()
-      local stats = require("lazy").stats()
-      local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-      dashboard.section.footer.val = "Loaded " .. stats.count .. " plugins in " .. ms .. "ms"
-      pcall(vim.cmd.AlphaRedraw)
-    end,
-  })
+--  vim.api.nvim_create_autocmd("User", {
+--    pattern = "LazyVimStarted",
+--    callback = function()
+--      local stats = require("lazy").stats()
+--      local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+--      dashboard.section.footer.val = "Loaded " .. stats.count .. " plugins in " .. ms .. "ms"
+--      pcall(vim.cmd.AlphaRedraw)
+--    end,
+--  })
 
   vim.api.nvim_create_autocmd({ "User" }, {
     pattern = { "AlphaReady" },
