eb704dfa3685685007e6872d4fe3dd65a1fc10dc
Toplica Tanasković <toplicius@gmail.com>
2024-08-18 21:03:55 +0200

    Added colorschemes

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # My daily driver Neovim configuration
+
+This configuration is a result of many experiments with various distributions,
+kickstart, lazynvim and my own attemtps to tailor neovim config to my needs.
+
--- lua/user/lazy.lua
+++ lua/user/lazy.lua
@@ -1,20 +1,19 @@
+-- [[ Install `lazy.nvim` plugin manager ]]
+--  See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
-end
+if not vim.uv.fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		error("Error cloning lazy.nvim:\n" .. out)
+	end
+end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 	spec = LAZY_PLUGIN_SPEC,
 	install = {
-		colorscheme = { "kanagawa", "default" },
+		colorscheme = { "tokyonight-night", "kanagawa-dragon", "default" },
 	},
 	ui = {
 		border = "rounded",
