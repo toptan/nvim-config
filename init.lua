b8f57183400a0dbcdd84412be315b4fcd4a66de2
Toplica Tanasković <toplicius@gmail.com>
2024-08-18 21:16:34 +0200

    Added which-key, toggle-term and todo plugins

--- init.lua
+++ init.lua
@@ -3,9 +3,17 @@ require("user.options")
 require("user.keymaps")
 require("user.autocmds")
 
+-- Load colorschemes first
 spec("user.colorscheme.tokyonight")
 spec("user.colorscheme.kanagawa")
 
+-- Load plugins
+spec("user.plugins.toggleterm")
+spec("user.plugins.whichkey")
+spec("user.plugins.todo")
+
+-- NOTE: This has to come after all plugins are specified.
 require("user.lazy")
 
+-- TODO: Figure out a better way to set colorscheme.
 vim.cmd("colorscheme tokyonight-night")
