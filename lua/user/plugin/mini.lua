e797afd0b4b563117fff133b3412df5d062e331a
Toplica Tanasković <toplicius@gmail.com>
2024-09-02 00:13:43 +0200

    Added mini.clue and mini.statusline

--- lua/user/plugin/mini.lua
+++ lua/user/plugin/mini.lua
@@ -10,6 +10,8 @@ function M.config()
   require("user.mini.pairs").config()
   require("user.mini.indentscope").config()
   require("user.mini.bufremove").config()
+  require("user.mini.clue").config()
+  require("user.mini.statusline").config()
 end
 
 return M
