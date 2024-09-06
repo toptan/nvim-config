d358c403011c46d9d52de208e9f9cf6ecb0cd858
Toplica Tanasković <toplicius@gmail.com>
2024-09-06 08:16:37 +0200

    Use ';' also to get into command mode.

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,6 +1,9 @@
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
+-- INFO: Using semicolon to enter command mode.
+vim.keymap.set("", ";", ":")
+
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<cr>")
