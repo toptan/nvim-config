9b47c5619c708220d012bac0249ceaecee3e590d
Toplica Tanasković <toplicius@gmail.com>
2024-09-09 23:49:37 +0200

    Fixed nvim-cmp config

--- lua/user/plugin/nvim-cmp.lua
+++ lua/user/plugin/nvim-cmp.lua
@@ -6,26 +6,10 @@ local M = {
             "hrsh7th/cmp-nvim-lsp",
             event = "InsertEnter",
         },
-        -- {
-        --     "hrsh7th/cmp-emoji",
-        --     event = "InsertEnter",
-        -- },
-        -- {
-        --     "hrsh7th/cmp-buffer",
-        --     event = "InsertEnter",
-        -- },
         {
             "hrsh7th/cmp-path",
             event = "InsertEnter",
         },
-        -- {
-        --     "hrsh7th/cmp-cmdline",
-        --     event = "InsertEnter",
-        -- },
-        -- {
-        --     "saadparwaiz1/cmp_luasnip",
-        --     event = "InsertEnter",
-        -- },
         {
             "L3MON4D3/LuaSnip",
             event = "InsertEnter",
@@ -34,9 +18,6 @@ local M = {
                 "rafamadriz/friendly-snippets",
             },
         },
-        -- {
-        --     "hrsh7th/cmp-nvim-lua",
-        -- },
     },
 }
 
@@ -71,11 +52,6 @@ function M.config()
                     vim_item.kind_hl_group = "CmpItemKindEmoji"
                 end
 
-                if entry.source.name == "cmp_tabnine" then
-                    -- vim_item.kind = icons.misc.Robot
-                    vim_item.kind_hl_group = "CmpItemKindTabnine"
-                end
-
                 return vim_item
             end,
         },
@@ -85,14 +61,31 @@ function M.config()
             ["<C-Space>"] = cmp.mapping.complete(),
             ["<C-e>"] = cmp.mapping.abort(),
             -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-            ["<cr>"] = cmp.mapping.confirm({ select = true }),
+            ["<cr>"] = cmp.mapping.confirm({
+                behavior = cmp.ConfirmBehavior.Insert,
+                select = true,
+            }),
+            ["<Tab>"] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                    cmp.select_next_item()
+                elseif require("luasnip").expand_or_jumpable() then
+                    require("luasnip").expand_or_jump()
+                else
+                    fallback()
+                end
+            end, { "i", "s" }),
+
+            ["<S-Tab>"] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                    cmp.select_prev_item()
+                elseif require("luasnip").jumpable(-1) then
+                    require("luasnip").jump(-1)
+                else
+                    fallback()
+                end
+            end, { "i", "s" }),
         }),
         sources = {
-            {
-                name = "lazydev",
-                -- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
-                group_index = 0,
-            },
             {
                 name = "nvim_lsp",
                 entry_filter = function(entry)
@@ -100,7 +93,11 @@ function M.config()
                         ~= entry:get_kind()
                 end,
             },
-            -- { name = 'luasnip' },
+            {
+                name = "lazydev",
+                -- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
+                group_index = 0,
+            },
             { name = "path" },
         },
         confirm_opts = {
