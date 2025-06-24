fd249072766fbff938447a6ea1898af3c37358a4
Toplica Tanasković <toplicius@gmail.com>
2025-06-25 00:49:20 +0200

    Added TODO comments from Folke

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -1,22 +1,23 @@
 local function telescope_config()
+  require("telescope").load_extension("fzf")
   local builtin = require("telescope.builtin")
   vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[F]iles" })
   vim.keymap.set("n", "<leader>ft", builtin.live_grep, { desc = "[T]ext" })
   vim.keymap.set("n", "<leader>cd", builtin.diagnostics, { desc = "[D]iagnostics" })
 end
+
+local function mini_icons_config()
+  local icons = require("mini.icons")
+  icons.mock_nvim_web_devicons()
+  icons.tweak_lsp_kind()
+end
+
 return {
   "nvim-telescope/telescope.nvim",
   dependencies = {
     { "nvim-lua/plenary.nvim" },
-    {
-      "echasnovski/mini.icons",
-      version = false,
-      config = function()
-        local icons = require("mini.icons")
-        icons.mock_nvim_web_devicons()
-        icons.tweak_lsp_kind()
-      end,
-    },
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    { "echasnovski/mini.icons", version = false, config = mini_icons_config },
   },
   config = telescope_config,
 }
