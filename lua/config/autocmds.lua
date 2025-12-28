3959304482e32ad06a65ac6d599237508f3cbc7e
Toplica Tanasković <toplicius@gmail.com>
2025-12-28 09:47:19 +0100

    Disabling mini.completion for snacks picker

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,11 +1,16 @@
 -- Highlight when yanking text
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
-  group = vim.api.nvim_create_augroup(
-    "kickstart-highlight-yank",
-    { clear = true }
-  ),
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
   callback = function()
     vim.hl.on_yank()
   end,
+})
+
+-- Disable mini.completion for snacks picker
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "snacks_picker_input",
+  desc = "Disable mini.completion for snacks picker",
+  group = vim.api.nvim_create_augroup("user_mini", { clear = true }),
+  command = "lua vim.b.minicompletion_disable=true",
 })
