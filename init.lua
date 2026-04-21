fac18502c7604d64e6c3b28dab3b7745a945bb3c
Toplica Tanasković <toplicius@gmail.com>
2026-04-21 23:40:19 +0200

    Initial commit

---
+++ init.lua
@@ -0,0 +1,44 @@
+-- Set colorscheme to catppuccin as it is now included in neovim
+vim.cmd.colorscheme("catppuccin")
+
+-- Define config table to be able to pass data between scripts
+-- It is a global variable which can be use both as `_G.Config` and `Config`
+_G.Config = {}
+
+-- Load now to have 'mini.misc' available for custom loading helpers.
+vim.pack.add({ "https://github.com/nvim-mini/mini.nvim" })
+
+local misc = require("mini.misc")
+Config.now = function(f)
+    misc.safely("now", f)
+end
+Config.later = function(f)
+    misc.safely("later", f)
+end
+Config.now_if_args = vim.fn.argc(-1) > 0 and Config.now or Config.later
+Config.on_event = function(ev, f)
+    misc.safely("event:" .. ev, f)
+end
+Config.on_filetype = function(ft, f)
+    misc.safely("filetype:" .. ft, f)
+end
+
+local gr = vim.api.nvim_create_augroup("custom-config", {})
+Config.new_autocmd = function(event, pattern, callback, desc)
+    local opts = { group = gr, pattern = pattern, callback = callback, desc = desc }
+    vim.api.nvim_create_autocmd(event, opts)
+end
+
+Config.on_packchanged = function(plugin_name, kinds, callback, desc)
+    local f = function(ev)
+        local name, kind = ev.data.spec.name, ev.data.kind
+        if not (name == plugin_name and vim.tbl_contains(kinds, kind)) then
+            return
+        end
+        if not ev.data.active then
+            vim.cmd.packadd(plugin_name)
+        end
+        callback()
+    end
+    Config.new_autocmd("PackChanged", "*", f, desc)
+end
