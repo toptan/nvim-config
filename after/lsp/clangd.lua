8da53a3c5b338a8de14fa09f403eb2b54549cebc
Toplica Tanasković <toplicius@gmail.com>
2026-06-09 23:12:16 +0200

    Fixing neocmakelsp and clangd arguments

--- after/lsp/clangd.lua
+++ after/lsp/clangd.lua
@@ -5,7 +5,7 @@ return {
         "--clang-tidy",
         "--header-insertion=iwyu",
         "--completion-style=detailed",
-        "--function-arg-placeholders",
+        "--function-arg-placeholders=1",
     },
     filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
     root_markers = { "compile_commands.json", "compile_flags.txt", ".git" },
--- after/lsp/neocmake.lua
+++ after/lsp/neocmake.lua
@@ -1,5 +1,5 @@
 return {
-    cmd = { "neocmakelsp", "--stdio" },
+    cmd = { "neocmakelsp", "stdio" },
     filetypes = { "cmake" },
     single_file_support = true, -- suggested
     init_options = {
