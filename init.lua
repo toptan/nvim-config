8bc9bc107628e90cf8ac95829d2929903e314f38
Toplica Tanasković <toplicius@gmail.com>
2026-06-11 23:49:14 +0200

    Initial setup

--- init.lua
+++ init.lua
@@ -1,38 +1,50 @@
 _G.Config = {}
 
-local gr = vim.api.nvim_create_augroup("custom-config", {})
+local gr = vim.api.nvim_create_augroup('custom-config', {})
 Config.new_autocmd = function(event, pattern, callback, desc)
-    local opts = { group = gr, pattern = pattern, callback = callback, desc = desc }
-    vim.api.nvim_create_autocmd(event, opts)
+  local opts = { group = gr, pattern = pattern, callback = callback, desc = desc }
+  vim.api.nvim_create_autocmd(event, opts)
 end
 
+-- Define custom `vim.pack.add()` hook helper. Plugin data is passed as
+-- argument to the callback. See `:h vim.pack-events`.
+-- Example usage: see 'plugin/40_plugins.lua'.
+-- If any plugin requires installation hooks, add them after this function
+-- and before the first `vim.pack.add()` call.
 Config.on_packchanged = function(plugin_name, kinds, callback, desc)
-    local f = function(ev)
-        local name, kind = ev.data.spec.name, ev.data.kind
-        if not (name == plugin_name and vim.tbl_contains(kinds, kind)) then
-            return
-        end
-        if not ev.data.active then
-            vim.cmd.packadd(plugin_name)
-        end
-        callback(ev.data)
-    end
-    Config.new_autocmd("PackChanged", "*", f, desc)
+  local f = function(ev)
+    local name, kind = ev.data.spec.name, ev.data.kind
+    if not (name == plugin_name and vim.tbl_contains(kinds, kind)) then return end
+    if not ev.data.active then vim.cmd.packadd(plugin_name) end
+    callback(ev.data)
+  end
+  Config.new_autocmd('PackChanged', '*', f, desc)
 end
 
-vim.pack.add({ "https://github.com/nvim-mini/mini.nvim" })
+-- 'mini.nvim' - all-in-one plugin powering most MiniMax features.
+-- See 'plugin/30_mini.lua' for how it is used.
+-- Load now to have 'mini.misc' available for custom loading helpers.
+vim.pack.add({ 'https://github.com/nvim-mini/mini.nvim' })
 
-local misc = require("mini.misc")
-Config.now = function(f)
-    misc.safely("now", f)
-end
-Config.later = function(f)
-    misc.safely("later", f)
-end
+-- Loading helpers used to organize config into fail-safe parts. Example usage:
+-- - `now` - execute immediately. Use for what must be executed during startup.
+--   Like colorscheme, statusline, tabline, dashboard, etc.
+-- - `later` - execute a bit later. Use for things not needed during startup.
+-- - `now_if_args` - use only if needed during startup when Neovim is started
+--   like `nvim -- path/to/file`, but otherwise delaying is fine.
+-- - Others are better used only if the above is not enough for good performance.
+--   Use only if you are comfortable with adding complexity to your config:
+--   - `on_event` - execute once on a first matched event. Like "delay until
+--     first Insert mode enter": `on_event('InsertEnter', function() ... end)`.
+--   - `on_filetype` - execute once on a first matched filetype. Like "delay
+--     until first Lua file": `on_filetype('lua', function() ... end)`.
+--
+-- See also:
+-- - `:h MiniMisc.safely()`
+-- - 'plugin/30_mini.lua' and 'plugin/40_plugins.lua'
+local misc = require('mini.misc')
+Config.now = function(f) misc.safely('now', f) end
+Config.later = function(f) misc.safely('later', f) end
 Config.now_if_args = vim.fn.argc(-1) > 0 and Config.now or Config.later
-Config.on_event = function(ev, f)
-    misc.safely("event:" .. ev, f)
-end
-Config.on_filetype = function(ft, f)
-    misc.safely("filetype:" .. ft, f)
-end
+Config.on_event = function(ev, f) misc.safely('event:' .. ev, f) end
+Config.on_filetype = function(ft, f) misc.safely('filetype:' .. ft, f) end
